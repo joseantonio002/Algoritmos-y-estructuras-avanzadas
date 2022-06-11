f961adca9e1417c5807b2830001f6c0abc57d788
José Antonio <72304309+joseantonio002@users.noreply.github.com>
2022-06-11 01:44:14 +0100

    Update shellsort.cc

--- shellsort.cc
+++ shellsort.cc
@@ -8,6 +8,7 @@ void shellsort(vector<int>& vector, float delta) {
   while(factor > 0) {
     for(int i = factor; i < size; i++) {
       int tempt = vector[i], j;
+      //mientras que queden posiciones detras (j >= factor) y el valor de la posición - factor este desordenado
       for(j = i; j >= factor && vector[j-factor]>tempt; 
       j -= factor) {
         vector[j] = vector[j-factor];
@@ -21,7 +22,7 @@ void shellsort(vector<int>& vector, float delta) {
 int main() {
   float delta;
   cin >> delta;
-  vector<int> vector = {452,1324,12,1243,45,0,2};
+  vector<int> vector = {452,1324,12,1243,45,0,2,23,78,9,1,3466,99,98,11,100,18,89,44};
   shellsort(vector, delta);
   for(int i = 0; i < vector.size(); i++) {
     cout << vector[i] << ' ';
