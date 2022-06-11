a33763140d2180f8a8ca146f4c3a606b4069042f
José Antonio <72304309+joseantonio002@users.noreply.github.com>
2022-06-11 18:10:51 +0100

    Create shakesort.cc

---
+++ shakesort.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<int> sec = {5,6,3,2,34,65,32,67,4,32,57,-1};
+  int size = sec.size(), maxpos = size - 1, aux;
+  bool changed = false;
+  for(int i = 0; i < size; i++) {
+    if(i%2==0) {
+      for(int j = 0; j < maxpos; j++) {
+        if(sec[j] > sec[j+1]) {
+          aux = sec[j];
+          sec[j] = sec[j+1];
+          sec[j+1] = aux;
+          changed = true;
+        }
+      }
+    }
+    else {
+      for(int j = maxpos; j > 0; j--) {
+        if(sec[j] < sec[j-1]) {
+          aux = sec[j];
+          sec[j] = sec[j-1];
+          sec[j-1] = aux;
+          changed = true;
+        }
+      }
+    }
+    if(!changed) break;
+    maxpos--;
+    changed = false;
+  }
+
+
+  for(int i = 0; i < size; i++) {
+    cout << sec[i] << ' ';
+  }
+  return 0;
+}
