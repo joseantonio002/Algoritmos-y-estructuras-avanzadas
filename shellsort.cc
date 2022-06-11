453bc2dd8f56ab54fafd0ce3eda6d2d2199d88dd
José Antonio <72304309+joseantonio002@users.noreply.github.com>
2022-06-11 23:03:12 +0100

    Update shellsort.cc

--- shellsort.cc
+++ shellsort.cc
@@ -2,33 +2,77 @@
 #include <vector>
 using namespace std;
 
-void shellsort(vector<int>& vector, float delta) {
-  int size = vector.size(), factor;
-  factor = delta * size;
-  while(factor > 0) {
-    for(int i = factor; i < size; i++) {
-      int tempt = vector[i], j;
-      //ver video https://www.geeksforgeeks.org/shellsort/
-      //mientras que queden posiciones detras (j >= factor) y el valor de la posición - factor este desordenado
-      for(j = i; j >= factor && vector[j-factor]>tempt; 
-      j -= factor) {
-        vector[j] = vector[j-factor];
-      }
-      vector[j] = tempt;
+//TECNICAMENTE NO FUNCIONA PERO CREO QUE COMO PSEUDOCODIGO ESTA BIEN
+
+/*
+void Mezcla (vector<int>& sec,int ini,int cen,int fin){ 
+  int posizq = ini, posder = cen+1, posaux = 0;
+  vector<int> aux(sec.size());
+  while(posizq <= cen && posder <= fin) {
+    if(sec[posizq] < sec[posder]) {
+      aux[posaux] = sec[posizq];
+      posizq++;
+      posaux++;
     }
-    factor = factor * delta;
+    else {
+      aux[posaux] = sec[posder];
+      posder--;
+      posaux++;
+    }
   }
+  if(posizq > cen) { //si falta por colocar elementos del trozo derecho
+    for(int i = posder; i <= fin; i++) {
+      aux[posaux] = sec[i];
+      posaux++;
+    }  
+  }
+  if(posder > fin) { //si falta por colocar elementos del trozo izquierdo
+    for(int i = posizq; i <= cen; i++) {
+      aux[posaux] = sec[i];
+      posaux++;
+    }  
+  }
+  for(int i = ini; i <= fin; i++) {
+    sec[i] = aux[i];
+  }
+  
+}*/
+
+
+void Mezcla (vector<int>& sec, int ini,int cen,int fin){ 
+  int i = ini, j = cen+1;
+  vector<int> aux(sec.size());
+  for (int k = ini; k <= fin; k++){
+    if ((i < cen && j > fin)||( sec[i] < sec[j])){
+      aux[k] = sec[i];
+      i++;
+    }
+    else { 
+      aux[k] = sec[j];
+      j = j - 1;
+    }
+  }
+  for (int k = ini; k <= fin; k++)
+    sec[k] = aux[k] ;
 }
 
+void Msort (vector<int>& sec,int ini,int fin){
+    if (ini < fin){
+       int cen = (ini + fin) / 2 ;
+      //ordena primera mitad
+       Msort(sec, ini, cen) ;
+      //ordena segunda mitad
+       Msort(sec, cen+1, fin) ;
+      //mezcla ambas listas ordenadas
+       Mezcla(sec, ini, cen, fin) ;
+    } 
+} 
+
 int main() {
-  float delta;
-  cin >> delta;
-  vector<int> vector = {452,1324,12,1243,45,0,2,23,78,9,1,3466,99,98,11,100,18,89,44};
-  shellsort(vector, delta);
-  for(int i = 0; i < vector.size(); i++) {
-    cout << vector[i] << ' ';
-  }
-  cout << endl;
-  
-  return 0;
+    vector<int> v = {45,2,34,65,89,0,4,46,49,42,10244,234,8};
+   Msort(v, 0, v.size() - 1);
+   for(int i = 0; i < v.size(); i++) {
+     cout << v[i] <<' ';
+   }
+    return 0;
 }
